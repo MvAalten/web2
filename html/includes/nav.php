d4728cd819c7e2b160966962a4e4dcb880428a26
MvAalten <157478216+MvAalten@users.noreply.github.com>
2024-05-28 11:14:48 +0200

    verkeerde mapje oopsie

---
+++ html/includes/nav.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+    <div class="container">
+        <div class="nav-left">
+            <a>Lucky Travel</a>
+        </div>
+        <div class="nav-right">
+            <a>Home</a>
+            <a>Bookings</a>
+            <a href="../pages/over_ons.php">Over ons</a>
+            <a>Contact</a>
+            <a>Login</a>
+        </div>
+    </div>
+    <div class="img-place">
+        
+    </div>
+</body>
+</html>
