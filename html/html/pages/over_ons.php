8c6e69f147222bd9f85144a06b011bed52bd72fb
AmirelIbrahimi <amir.okido@gmail.com>
2024-06-10 14:12:33 +0200

    over ons gefixt

--- html/html/pages/over_ons.php
+++ html/html/pages/over_ons.php
@@ -10,11 +10,62 @@
     <?php
      include('../includes/nav.php')
     ?>
-    <div class="lower-body center">
+    <main>
+    <div class="over-ons-bedrijf-container">
+            <div class="over-ons-bedrijf-titel">
+                <p>Over ons bedrijf</p>
+                <div class="extra-line-container">
+                <div class="extra-line">
+                </div>
+                </div>
+            </div>
+                <div class="over-ons-bedrijf-tekst">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis eleifend quam adipiscing
+                    vitae proin sagittis nisl. Pellentesque habitant morbi tristique senectus et.
+                    Ornare suspendisse sed nisi lacus sed viverra tellus in hac. Quisque id diam vel quam elementum pulvinar. 
+                    Pellentesque habitant morbi tristique senectus et netus et. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis. 
+                    Dictum at tempor commodo ullamcorper a lacus vestibulum. Laoreet id donec ultrices tincidunt arcu non. Tempus iaculis urna id volutpat lacus laoreet. 
+                    Ultrices in iaculis nunc sed augue lacus viverra vitae congue. Vulputate sapien nec sagittis aliquam. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. 
+                    Mauris commodo quis imperdiet massa tincidunt. Cursus sit amet dictum sit. Proin fermentum leo vel orci porta non. 
+                    Venenatis a condimentum vitae sapien pellentesque habitant. Adipiscing enim eu turpis egestas pretium aenean. Nisl pretium fusce id velit ut tortor pretium viverra suspendisse. 
+                    Elementum curabitur vitae nunc sed velit dignissim. Odio facilisis mauris sit amet massa vitae tortor condimentum. Et pharetra pharetra massa massa. Sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. 
+                    Neque ornare aenean euismod elementum nisi. Maecenas ultricies mi eget mauris pharetra et ultrices.
+                    Vulputate mi sit amet mauris commodo quis imperdiet massa. Cursus metus aliquam eleifend mi. Dui id ornare arcu odio ut sem nulla pharetra diam. 
+                    Ut lectus arcu bibendum at varius vel. Nullam ac tortor vitae purus faucibus ornare 
+                    </p>
+                </div>
+            </div>
+            <div class="over-ons-bedrijf-container">
+            <div class="over-ons-bedrijf-titel">
+                <p>Over ons</p>
+                <div class="extra-line-container">
+                <div class="extra-line">
+                </div>
+                </div>
+            </div>
+                <div class="over-ons-bedrijf-tekst">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis eleifend quam adipiscing
+                    vitae proin sagittis nisl. Pellentesque habitant morbi tristique senectus et.
+                    Ornare suspendisse sed nisi lacus sed viverra tellus in hac. Quisque id diam vel quam elementum pulvinar. 
+                    Pellentesque habitant morbi tristique senectus et netus et. Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis. 
+                    Dictum at tempor commodo ullamcorper a lacus vestibulum. Laoreet id donec ultrices tincidunt arcu non. Tempus iaculis urna id volutpat lacus laoreet. 
+                    Ultrices in iaculis nunc sed augue lacus viverra vitae congue. Vulputate sapien nec sagittis aliquam. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. 
+                    Mauris commodo quis imperdiet massa tincidunt. Cursus sit amet dictum sit. Proin fermentum leo vel orci porta non. 
+                    Venenatis a condimentum vitae sapien pellentesque habitant. Adipiscing enim eu turpis egestas pretium aenean. Nisl pretium fusce id velit ut tortor pretium viverra suspendisse. 
+                    Elementum curabitur vitae nunc sed velit dignissim. Odio facilisis mauris sit amet massa vitae tortor condimentum. Et pharetra pharetra massa massa. Sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. 
+                    Neque ornare aenean euismod elementum nisi. Maecenas ultricies mi eget mauris pharetra et ultrices.
+                    Vulputate mi sit amet mauris commodo quis imperdiet massa. Cursus metus aliquam eleifend mi. Dui id ornare arcu odio ut sem nulla pharetra diam. 
+                    Ut lectus arcu bibendum at varius vel. Nullam ac tortor vitae purus faucibus ornare 
+                    </p>
+                </div>
+            </div>
+    </main>
+    <!-- <div class="lower-body center">
         <div class="black">
             <a>Over ons bedrijf</a>
             <div class="extra-line">
-
             </div>
             <br>
             <br>
@@ -22,7 +73,7 @@
             Vulputate sapien nec sagittis aliquam. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Mauris commodo quis imperdiet massa tincidunt. Cursus sit amet dictum sit. Proin fermentum leo vel orci porta non. Venenatis a condimentum vitae sapien pellentesque habitant. Adipiscing enim eu turpis egestas pretium aenean. Nisl pretium fusce id velit ut tortor pretium viverra suspendisse. Elementum curabitur vitae nunc sed velit dignissim. Odio facilisis mauris sit amet massa vitae tortor condimentum. Et pharetra pharetra massa massa. Sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Neque ornare aenean euismod elementum nisi. Maecenas ultricies mi eget mauris pharetra et ultrices.
             Vulputate mi sit amet mauris commodo quis imperdiet massa. Cursus metus aliquam eleifend mi. Dui id ornare arcu odio ut sem nulla pharetra diam. Ut lectus arcu bibendum at varius vel. Nullam ac tortor vitae purus faucibus ornare </p>
         </div>
-        <div class="black">
+        <div class="black2">
             <a>Over ons</a>
             <div class="extra-line">
             </div>
@@ -32,6 +83,6 @@
             Vulputate sapien nec sagittis aliquam. Eu non diam phasellus vestibulum lorem sed risus ultricies tristique. Mauris commodo quis imperdiet massa tincidunt. Cursus sit amet dictum sit. Proin fermentum leo vel orci porta non. Venenatis a condimentum vitae sapien pellentesque habitant. Adipiscing enim eu turpis egestas pretium aenean. Nisl pretium fusce id velit ut tortor pretium viverra suspendisse. Elementum curabitur vitae nunc sed velit dignissim. Odio facilisis mauris sit amet massa vitae tortor condimentum. Et pharetra pharetra massa massa. Sed viverra tellus in hac habitasse platea dictumst vestibulum rhoncus. Neque ornare aenean euismod elementum nisi. Maecenas ultricies mi eget mauris pharetra et ultrices.
             Vulputate mi sit amet mauris commodo quis imperdiet massa. Cursus metus aliquam eleifend mi. Dui id ornare arcu odio ut sem nulla pharetra diam. Ut lectus arcu bibendum at varius vel. Nullam ac tortor vitae purus faucibus ornare </p>
         </div>
-    </div>
+    </div> -->
 </body>
 </html>
