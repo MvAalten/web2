6bb6018d08379a1c65f4008c16fd275430d3cc90
Deniz278 <deun278@gmail.com>
2024-06-10 13:35:04 +0200

    img toegevoegd en wat andere dingen

--- html/html/includes/nav.php
+++ html/html/includes/nav.php
@@ -5,11 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="../css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Road+Rage&display=swap" rel="stylesheet">
 </head>
 <body>
     <div class="container">
         <div class="nav-left">
-            <a>Lucky Travel</a>
+            <img src="\assets\logo.png" class="logo">
+            <a class="logo-text">Lucky Travel</a>
         </div>
         <div class="nav-right">
             <a href="../index.php">Home</a>
@@ -21,7 +25,7 @@
         </div>
     </div>
     <div class="img-place">
-        
+        <img src="\assets\universalimage.png" alt="" class="universalimage">
     </div>
 </body>
 </html>
