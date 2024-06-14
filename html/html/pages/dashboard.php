aa30b19cea4ff022f0fc2e3fe56a753ae8a61574
Deniz278 <deun278@gmail.com>
2024-06-14 14:30:58 +0200

    changes to pages -deniz
    check figma voor design changes (sorry)

--- html/html/pages/dashboard.php
+++ html/html/pages/dashboard.php
@@ -11,15 +11,16 @@ if (!isset($_SESSION["user"])){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>admin dashboard</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <?php
-     include('../includes/nav.php')
-    ?>
-    <main>
+    <div class="container">
 
-    </main>
+    </div>
 </body>
 </html>
 
--- html/html/pages/dashboarduser.php
+++ html/html/pages/dashboarduser.php
@@ -1,19 +1,28 @@
-<!-- <?php
+<?php
 session_start();
 if (!isset($_SESSION["user"])){
     header("Location: login.php");
     exit();
     // als je niet bent in gelogd dan word je verwezen naar de login page
 }
-?> -->
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>user dashboard</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" rel="stylesheet">
 </head>
 <body>
-    
+    <div class="container-user-dashboard"> 
+        <div class="container-text-header">
+            <a class="text-header-user-dashboard">User dashboard</a>
+        </div>
+    </div>
 </body>
 </html>
+
