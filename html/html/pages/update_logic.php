2b40cddd7d83235b6f2cd708cfc023c96143a001
AmirelIbrahimi <amir.okido@gmail.com>
2024-06-21 22:00:37 +0200

    kleine probleem hopelijk gefixt voor Mees

--- html/html/pages/update_logic.php
+++ html/html/pages/update_logic.php
@@ -2,12 +2,13 @@
 include 'conn.php';
 
 $data = [
+    'id' => $_POST['id'],
     'username' => $_POST['username'],
     'password' => $_POST['password'],
     'email' => $_POST['email'],
     'usertype' => $_POST['usertype'],
 ];
-$sql = "UPDATE users SET username=:username, password=:password, email=:email, usertype=:usertype where id=:id";
+$sql = "UPDATE users SET username=:username, password=:password, email=:email, usertype=:usertype WHERE id=:id";
 $stmt= $connection->prepare($sql);
 $stmt->execute($data);
 header('Location: dashboard.php');
